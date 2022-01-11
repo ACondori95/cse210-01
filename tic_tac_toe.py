8ca99547f5c2fa3fe74e643b4a32da603c5b4bbf
ACondori95 <con21033@byui.edu>
2022-01-11 12:06:09 -0300

    create bounds

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -43,8 +43,21 @@ def quit(user_input):
 
 def check_input(user_input):
     # check if it is a nummber
-    isnum(user_input)
+    if not isnum(user_input):
+        return False
+    user_input = int(user_input)
     # check if it is 1-9
+    if not bounds(user_input):
+        return False
+    return True
+
+
+def bounds(user_input):
+    if user_input > 9 or user_input < 1:
+        print("This number is out of bounds")
+        return False
+    else:
+        return True
 
 
 def isnum(user_input):
@@ -56,9 +69,11 @@ def isnum(user_input):
 
 
 while True:
+    print_board(board)
     user_input = input(
         "Please enter a position 1 through 9 or enter 'q' to quit: ")
     if quit(user_input):
         break
     if not check_input(user_input):
         print("Please try again")
+        continue
