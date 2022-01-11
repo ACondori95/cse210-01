16d9e19254abad08d63e57faa3174f7e5ec24c65
ACondori95 <con21033@byui.edu>
2022-01-11 11:12:08 -0300

    check input

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -34,13 +34,31 @@ print_board(board)
 
 
 def quit(user_input):
-    if user_input == 'q':
+    if user_input.lower() == 'q':
         print("Thanks for playing")
         return True
     else:
         return False
 
 
+def check_input(user_input):
+    # check if it is a nummber
+    isnum(user_input)
+    # check if it is 1-9
+
+
+def isnum(user_input):
+    if not user_input.isnumeric():
+        print("This is not a valid number")
+        return False
+    else:
+        return True
+
+
 while True:
     user_input = input(
         "Please enter a position 1 through 9 or enter 'q' to quit: ")
+    if quit(user_input):
+        break
+    if not check_input(user_input):
+        print("Please try again")
