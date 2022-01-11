913cf96039bc9f36ccc8e2f9e8c7c757f947093a
ACondori95 <con21033@byui.edu>
2022-01-11 10:54:25 -0300

    create board

---
+++ tic_tac_toe.py
@@ -0,0 +1,23 @@
+# tic tac toe
+
+"""
+tic tac toe board
+[
+  [-, -, -],
+  [-, -, -],
+  [-, -, -]
+]
+
+user_input -> something 1-9
+if they enter anything else: tell them to go again
+check if the user_input is already taken
+add itto the board
+check if user won: checking rows, columns and diagonals
+toggle between users upon successful moves
+"""
+
+board = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+]
